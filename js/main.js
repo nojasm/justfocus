297c50a5c7511ec866f2de4d2b48b1bff5d9a6c1
nojasm <jonas.mertens@posteo.de>
2024-05-02 10:00:41 +0200

    Add styles

--- js/main.js
+++ js/main.js
@@ -15,6 +15,72 @@ let t0 = null;
 
 let timeInterval = null;
 
+let styles = {
+    "retro": {
+        name: "Retro",
+        style: {
+            "--background": ["#780000", "#600"],
+            "--main": ["#C1121F", "#D1323F"],
+            "--fg": ["#FDF0D5", "#fff"],
+            "--task-button": ["#003049", "#023"],
+            "--start-button": ["#780000", "#600"],
+            "--start-button-filter": ["invert(1)", "invert(1)"],
+            "--task-bg": ["#780000"],
+            "--task-fg": ["#ddd"]
+        }
+    },
+    "cute": {
+        name: "Cute 🌟",
+        style: {
+            "--background": ["#A2D2FF", "#A2B2DF"],
+            "--main": ["#BDE0FE", "#CDF9FF"],
+            "--fg": ["#7292BF"],
+            "--task-button": ["#82A2CF"],
+            "--task-button-fg": ["white"],
+            "--start-button": ["white", "#A2B2DF"],
+            "--start-button-filter": ["invert(0)", "invert(1.0)"],
+            "--task-bg": ["#82A2CF"],
+            "--task-fg": ["white"]
+        }
+    },
+    "contrast": {
+        name: "Contrast",
+        style: {
+            "--background": ["white", "#CCC"],
+            "--main": ["#ddd", "#AAA"],
+            "--fg": ["black"],
+            "--task-button": ["#222"],
+            "--task-button-fg": ["white"],
+            "--start-button": ["#555"],
+            "--start-button-filter": ["invert(1)", "invert(0.7)"],
+            "--task-bg": ["#bbb"],
+            "--task-fg": ["#333"]
+        }
+    }
+}
+
+document.getElementById("style").addEventListener("change", (event) => {
+    if (timerIsRunning) styleRunning();
+    else styleDefault();
+})
+
+Object.keys(styles).forEach((s) => {
+    let opt = document.createElement("option");
+    opt.value = s;
+    opt.innerText = styles[s].name;
+    document.getElementById("style").appendChild(opt);
+});
+
+document.getElementById("style").value = "contrast";
+
+function changeStyle(style, i) {
+    Object.keys(style).forEach((s) => {
+        let sss = style[s][i];
+        if (sss == undefined || sss == null) sss = style[s][0];
+        var rs = document.querySelector(":root").style.setProperty(s, sss);
+    });
+}
+
 timeMinutesEl.addEventListener("change", (event) => {
     timeMinutes = parseInt(event.target.value);
 
@@ -43,17 +109,15 @@ function setTimeText(min, sec) {
 
 // Change page colors when timer is running
 function styleRunning() {
-    document.body.style.backgroundColor = "rgb(140, 115, 120)";
-    document.getElementById("main").style.backgroundColor = "rgb(170, 127, 135)";
-    document.getElementById("main").style.filter = "contrast(0.8)";
+    changeStyle(styles[document.getElementById("style").value].style, 1);
 }
 
 // Reset page style when timer is not running
 function styleDefault() {
-    document.body.style.backgroundColor = "rgb(192, 84, 105)";
-    document.getElementById("main").style.backgroundColor = "rgb(218, 119, 139)";
-    document.getElementById("main").style.filter = "contrast(1.0)";
+    changeStyle(styles[document.getElementById("style").value].style, 0);
 }
+
+styleDefault();
 
 setTimeText(timeMinutes, timeSeconds);
 
@@ -94,7 +158,18 @@ btnStart.addEventListener("click", (event) => {
 
             let showMinutes = Math.floor(secondsLeft / 60);
             let showSeconds = Math.floor(secondsLeft - showMinutes * 60);
+            if (showSeconds % 2 == 0) {
+                document.getElementById("colon").style.opacity = "1";
+            } else {
+                document.getElementById("colon").style.opacity = "0.6";
+            }
             setTimeText(showMinutes, showSeconds);
         }, 50);
+    }
+});
+
+window.addEventListener("keydown", (event) => {
+    if (event.code == "Space") {
+        btnStart.click();
     }
 });
